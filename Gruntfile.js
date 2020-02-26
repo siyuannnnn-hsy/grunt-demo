58e5985569c8a80936571d00e18c3c3fd62e9b31
siyuannnnn-hsy <1587935941@qq.com>
2020-02-26 08:30:02 +0800

    selint

---
+++ Gruntfile.js
@@ -0,0 +1,16 @@
+/* global module: true */
+module.exports = function(grunt){
+    grunt.initConfig({
+        eslint:{
+            options:{
+                eslintrc:'.eslintrc.json',
+            },
+            target:['*.js']
+        }
+        
+    });
+
+    grunt.loadNpmTasks('grunt-eslint');
+
+    grunt.registerTask('default',['eslint'])
+};
